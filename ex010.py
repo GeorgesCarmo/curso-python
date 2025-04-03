b507b8300ae15b5fa7701391aeda4ea21ccb58b3
georges <georgesdc13@gmail.com>
2025-04-03 15:05:52 -0300

    exercicios

---
+++ ex010.py
@@ -0,0 +1,4 @@
+# Escreva um programa que converta de Celsius para Fahrenheit.
+c = float(input('Digite a temperatura em Celsius: '))
+f = (c * 9 / 5) + 32
+print('A temperatura em Fahrenheit é: {:.2f}'.format(f))
